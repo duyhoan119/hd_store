81f7c8efdb4edc9ea09694f2bdd1b8606cd74e8a
lo duy hoan <duyhoan3721@gmail.com>
2024-05-02 13:48:16 +0700

    update detail product

--- resources/views/Client/product/detail.blade.php
+++ resources/views/Client/product/detail.blade.php
@@ -1,7 +1,12 @@
 @extends('Client.index')
 @section('title', $product->name)
 @section('show_content')
-    <div>
-        {{$product->name}}
+    <div class="grid grid-cols-12">
+        <div class="col-span-9">
+            <img class="object-cover w-full" src="{{$product->image}}" alt="">
+        </div>
+        <div class="col-span-3">
+            <p class="text-xl font-semibold uppercase">{{$product->name}}</p>
+        </div>
     </div>
 @endsection
